0dc9720afe0dbad7e8c3772f41cf9552f931c4df
brazeen <117746210+brazeen@users.noreply.github.com>
2024-07-11 21:33:37 +0800

    fix: fixed invisible characters that caused syntax errors

--- main.py
+++ main.py
@@ -15,45 +15,40 @@ async def on_ready():
 # Display a Welcome Message to a User when they join the Server
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(BOT_CHANNEL)
+    channel = bot.get_channel(int(os.getenv('BOT_CHANNEL')))
     await channel.send("Welcome {}".format(member.mention))
 
 # Display a Leaving Message when a User leaves the Server
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(BOT_CHANNEL)
+    channel = bot.get_channel(int(os.getenv('BOT_CHANNEL')))
     await channel.send("Why did you leave us?!")
 
 # Slash command '/ping' to display the bot's latency
-@bot.command(description="Sends the bot's latency.")​
-async def ping(ctx): # a command called "ping" will be made​
-    await ctx.respond(f"Latency is {bot.latency}")
+@bot.slash_command(description="Sends the bot's latency.")
+async def ping(ctx): # a command called "ping" will be made
+    await ctx.respond(f"Latency is {bot.latency} ms")
 
 # Slash command '/join' with two str parameters that will join two strings together and return the joined string
-@bot.command(description="joins two strings") ​
-async def join(ctx, string1: str, string2: str): ​
-    finalstr = string1 + string2 ​
-    await ctx.respond(f"the final string is {finalstr}")
+@bot.slash_command(description="Joins two strings")
+async def join(ctx, string1: str, string2: str):
+    finalstr = string1 + string2
+    await ctx.respond(f"The final string is {finalstr}")
 
-# create Slash Command group with bot.create_group​
-fun = bot.create_group("fun", "fun commands!!")​
-# instead of @bot.command, use @{groupname}.command
+# Create Slash Command group with bot.create_group
+fun = bot.create_group("fun", "Fun commands!!")
 
-# fun command examples that discord bot can do (import random first!!)
-# chooses between two strings randomly and displays the chosen string
-@fun.command(description="chooses randomly from choice 1 and 2")​
-async def choose(ctx, choice1: str, choice2: str):​
-    randomint = random.randint(1,2)​
-    if randomint == 1:​
-        choice = choice1​
-    else:​
-        choice = choice2​
-    await ctx.respond(f"The final choice is {choice}!!!!!")​
+# Fun command examples that discord bot can do (import random first!!)
+# Chooses between two strings randomly and displays the chosen string
+@fun.command(description="Chooses randomly from choice 1 and 2")
+async def choose(ctx, choice1: str, choice2: str):
+    choice = random.choice([choice1, choice2])
+    await ctx.respond(f"The final choice is {choice}!!!!!")
 
-# roll a dice and get the result (from 1 to 6)
-@fun.command(description="rolls a six-sided dice")​
-async def dice(ctx):​
-    result = random.randint(1,6)​
+# Roll a dice and get the result (from 1 to 6)
+@fun.command(description="Rolls a six-sided dice")
+async def dice(ctx):
+    result = random.randint(1, 6)
     await ctx.respond(f"...and you rolled a {result}!!")
 
 # Get the Current Weather Forecast for Singapore and Display it
